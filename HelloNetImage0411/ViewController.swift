dac7de19d02096b2e457232a2ce2a60474057c26
Danny Shen <shenfive@gmail.com>
2020-04-11 11:54:48 +0800

    image from net

--- HelloNetImage0411/ViewController.swift
+++ HelloNetImage0411/ViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var theImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        do {
+            if let url = URL(string: "https://lh3.googleusercontent.com/y1P59GUBWjvQ22AhJPaVAgmsietknCIesTgo7C2uiW_4nTHn11Hfq1pLjDcJfwYccAK11A=s85"){
+                
+                
+                let imageData = try Data(contentsOf: url)
+                theImageView.image = UIImage(data: imageData)
+            }
+            
+        } catch  {
+            print(error.localizedDescription)
+        }
+        
+        
+        
+        
     }
 
 
